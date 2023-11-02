955c64f0ac389a2273254dbb34911b8bcf40d6ce
franc <franc@LAPTOP-HCMP83CB>
2023-11-02 20:40:25 -0300

    Agregue prints asdasdas

--- Ejercicios/src/ejercicioPicante/MainMenu.java
+++ Ejercicios/src/ejercicioPicante/MainMenu.java
@@ -5,7 +5,10 @@ public class MainMenu {
 	public static void main(String[] args) {
 		int codigo = 3445;
 	//Exepciones.esEntero((String)codigo);
-
+		System.out.println("asdasdasdasdas");
+		System.out.println("asdasdasdasdas");System.out.println("asdasdasdasdas");
+		System.out.println("asdasdasdasdas");
+		System.out.println("asdasdasdasdas");
 	}
 
 }
